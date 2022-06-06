33c707120a044c2649a2cd9ce0a8599b28b669cf
jamolina13 <88411328+jamolina13@users.noreply.github.com>
2022-06-06 10:59:20 -0500

    Primer interfaz

---
+++ src/app/indice-barthel/indice-barthel.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-indice-barthel',
+  templateUrl: './indice-barthel.component.html',
+  styleUrls: ['./indice-barthel.component.scss']
+})
+export class IndiceBarthelComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
