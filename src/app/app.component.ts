bc5ecafcbf7a60c9c8bdb9d55c6d6eee0e27e11a
abdieljz <juniorjahily@hotmail.com>
2022-06-06 11:00:19 -0500

    D
    D

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'MIESS_APP';
+  title = 'ADULTO_MAYOR';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ADULTOMAYORComponent } from './adulto-mayor/adulto-mayor.component';
+import {MatToolbarModule} from '@angular/material/toolbar';
+
 
 @NgModule({
+  
   declarations: [
-    AppComponent
+    AppComponent,
+    ADULTOMAYORComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
