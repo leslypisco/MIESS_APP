cf5b9cd6ba99aa52fa6ecfc817c56a459dffa61d
jasanchez32 <49045765+jasanchez32@users.noreply.github.com>
2022-06-10 02:42:23 -0500

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { EscalaYesavageComponent } from './escala_yesavage/escala-yesavage/escala-yesavage.component';
-import { TestEscalaYesavageComponent } from './escala_yesavage/test-escala-yesavage/test-escala-yesavage.component';
+import { EscalaYesavageComponent } from './escala-yesavage/escala-yesavage.component';
+import { TestEscalaYesavageComponent } from './test-escala-yesavage/test-escala-yesavage.component';
 
 const routes: Routes = [
   {path: 'escala-yesavage', component:EscalaYesavageComponent},
