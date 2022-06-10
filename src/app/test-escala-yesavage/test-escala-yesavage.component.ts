2aa0ad8e39ea832629a40de3f87242521f873d44
belandeta <94236666+belandeta@users.noreply.github.com>
2022-06-09 21:16:34 -0500

    Add files via upload

--- src/app/test-escala-yesavage/test-escala-yesavage.component.ts
+++ src/app/test-escala-yesavage/test-escala-yesavage.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-test-escala-yesavage',
@@ -7,9 +8,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TestEscalaYesavageComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+
+  constructor( private formBuilder:FormBuilder ) {
+    this.buildForm();
+  }
 
   ngOnInit(): void {
+  }
+
+  buildForm() {
+    this.form = this.formBuilder.group({
+      pregunta1: new FormControl('', [Validators.required]),
+      pregunta2: new FormControl('', [Validators.required]),
+      pregunta3: new FormControl('', [Validators.required]),
+      pregunta4: new FormControl('', [Validators.required]),
+      pregunta5: new FormControl('', [Validators.required]),
+      pregunta6: new FormControl('', [Validators.required]),
+      pregunta7: new FormControl('', [Validators.required]),
+      pregunta8: new FormControl('', [Validators.required]),
+      pregunta9: new FormControl('', [Validators.required]),
+      pregunta10: new FormControl('', [Validators.required]),
+      pregunta11: new FormControl('', [Validators.required]),
+      pregunta12: new FormControl('', [Validators.required]),
+      pregunta13: new FormControl('', [Validators.required]),
+      pregunta14: new FormControl('', [Validators.required]),
+      pregunta15: new FormControl('', [Validators.required]),
+    });
+  }
+
+  save(event: Event) {
+    event.preventDefault();
+
+    if(this.form.valid) {
+      const value = this.form.value;
+      console.log(value);
+    } else {
+      this.form.markAllAsTouched();
+      alert("Existen campos sin llenar, debe llenar todos los campos.");
+    }
   }
 
 }
