798703a1b30883959a6b01cc0ef03261a4373d25
jasanchez32 <49045765+jasanchez32@users.noreply.github.com>
2022-06-10 02:45:03 -0500

    Create escala-yesavage.component.ts

---
+++ src/app/escala-yesavage/escala-yesavage.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-escala-yesavage',
+  templateUrl: './escala-yesavage.component.html',
+  styleUrls: ['./escala-yesavage.component.scss']
+})
+export class EscalaYesavageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
