b86b14a27b539789f4c89c3ded16bc9ad9bfb3ed
bacastro1 <bacastro1@espe.edu.ec>
2022-06-10 05:03:30 -0500

    Mostrar usuarios

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,31 +6,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'MIESS_APP';
-  name = 'MIESS_APP';
-  products = [{
-    'id': 1,
-    'nombre': 'CRAVIOTO QUINTANA ALEJANDRO RAFAEL',
-    'telefono': 1234567890
-  }, {
+  usuarios = {
+    persona1 : {
+      'id': 1,
+      'nombre': 'CRAVIOTO QUINTANA ALEJANDRO RAFAEL',
+      'telefono': 1234567890,
+  },
+  persona2 : {
     'id': 2,
     'nombre': 'ESCOBAR BRIONES CARALINA LUCÍA',
-    'telefono': 6857493849
-  }, {
+    'telefono': 6857493849,
+  },
+  persona3 : {
     'id': 3,
     'nombre': 'BARROSO VILLA JUAN',
-    'telefono': 5837584939
-  }, {
+    'telefono': 5837584939,
+  },
+  persona4 : {
     'id': 4,
     'nombre': 'ESTRADA GARCÍA MARÍA TERESA',
-    'telefono': 6758495839
-  }, {
+    'telefono': 6758495839,
+  },
+  persona5 : {
     'id': 5,
     'nombre': 'BECKER FAUSER INGEBORG DOROTHEA MARÍA',
-    'telefono': 5847394839
-  }, {
+    'telefono': 5847394839,
+  },
+  persona6 : {
     'id': 6,
     'nombre': 'FURUYA MEGURO MARÍA',
-    'telefono': 5739483939
-  }]; 
+    'telefono': 5739483939,
+  }
+  }; 
+  title = 'MIESS_APP';
+  //name = 'MIESS_APP';
+ 
 }
--- src/app/supervisor/listar-tecnicos/listar-tecnicos.component.ts
+++ src/app/supervisor/listar-tecnicos/listar-tecnicos.component.ts
@@ -11,5 +11,36 @@ export class ListarTecnicosComponent implements OnInit {
 
   ngOnInit(): void {
   }
-
+  usuarios = {
+    persona1 : {
+      'id': 1,
+      'nombre': 'CRAVIOTO QUINTANA ALEJANDRO RAFAEL',
+      'telefono': 1234567890,
+  },
+  persona2 : {
+    'id': 2,
+    'nombre': 'ESCOBAR BRIONES CARALINA LUCÍA',
+    'telefono': 6857493849,
+  },
+  persona3 : {
+    'id': 3,
+    'nombre': 'BARROSO VILLA JUAN',
+    'telefono': 5837584939,
+  },
+  persona4 : {
+    'id': 4,
+    'nombre': 'ESTRADA GARCÍA MARÍA TERESA',
+    'telefono': 6758495839,
+  },
+  persona5 : {
+    'id': 5,
+    'nombre': 'BECKER FAUSER INGEBORG DOROTHEA MARÍA',
+    'telefono': 5847394839,
+  },
+  persona6 : {
+    'id': 6,
+    'nombre': 'FURUYA MEGURO MARÍA',
+    'telefono': 5739483939,
+  }
+  }; 
 }
