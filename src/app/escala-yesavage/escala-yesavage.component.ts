84a912d785205dc4b1ad1f50fccd6da3c1a7c440
gcalcivar1 <73975664+gcalcivar1@users.noreply.github.com>
2022-07-26 23:30:06 -0500

    Add files via upload

--- src/app/escala-yesavage/escala-yesavage.component.ts
+++ src/app/escala-yesavage/escala-yesavage.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-escala-yesavage',
@@ -8,12 +9,19 @@ import { Component, OnInit } from '@angular/core';
 export class EscalaYesavageComponent implements OnInit {
 
   indice:string;
+  idEncabezado:any;
 
-  constructor() {
+
+  constructor(private router: Router) {
     this.indice = "Escala Yesavage"
+    this.idEncabezado = this.router.getCurrentNavigation()!.extras.state!['idEncabezado'];
   }
 
   ngOnInit(): void {
+    console.log("El id en test es:" + this.idEncabezado)
   }
 
+  enviar(){
+    this.router.navigate(['test-escala-yesavage'], {state: {idEncabezado: this.idEncabezado}});
+  }
 }
